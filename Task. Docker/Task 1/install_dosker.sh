21e1c3e8dbff8d123374ac75f775fd436e0b8502
ZEN10111 <97990456+ZEN10111@users.noreply.github.com>
2023-01-26 04:08:47 +0200

    Add files via upload

---
+++ Task. Docker/Task 1/install_dosker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
+
+#  Post install: add permision for curent user to run docker commands(need re-login a to apply the new rights)
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo -e "\nFor curent user $USER need re-login to run docker commands without sudo\n"
+
